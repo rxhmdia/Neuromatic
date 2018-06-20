ef892bf3449d0dbcfdfa90b01844b94beb114b6d
Willem Meints <willem.meints@gmail.com>
2018-06-20 07:25:57 +0200

    Add SGD optimization function

---
+++ src/Neuromatic/Optimizers/Optimizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TensorFlow;
+
+namespace Neuromatic.Optimizers
+{
+    /// <summary>
+    /// Defines an optimization function to be run when training a neural network
+    /// </summary>
+    public abstract class Optimizer
+    {
+        /// <summary>
+        /// Gets or sets the operations that the optimizer needs 
+        /// to execute for each training batch
+        /// </summary>
+        protected IEnumerable<TFOperation> Operations { get;set;}
+
+        /// <summary>
+        /// Compiles the optimizer
+        /// </summary>
+        /// <param name="graph">Graph to use for compilation</param>
+        /// <param name="loss">Loss function to use</param>
+        /// <param name="parameters">Parameters to optimize</param>
+        public abstract void Compile(TFGraph graph, TFOutput loss, IEnumerable<TFOutput> parameters);
+
+        /// <summary>
+        /// Executes the optimizer function for a set of inputs
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="inputs"></param>
+        public void Execute(TFSession session, Dictionary<TFOutput, Array> inputs)
+        {
+            var runner = session.GetRunner();
+
+            foreach(var keyValuePair in inputs)
+            {
+                runner.AddInput(keyValuePair.Key, new TFTensor(keyValuePair.Value));
+            }
+
+            runner.AddTarget(Operations.ToArray());
+            runner.Run();
+        }
+    }
+}
---
+++ src/Neuromatic/Optimizers/SGD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TensorFlow;
+
+namespace Neuromatic.Optimizers
+{
+    /// <summary>
+    /// Implements the standard stochastic gradient descent algoritm as an optimizer function.
+    /// </summary>
+    public class SGD : Optimizer
+    {
+        private double _learningRate;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SGD"/>
+        /// </summary>
+        /// <param name="learningRate">Learning rate</param>
+        public SGD(double learningRate = 0.01)
+        {
+            _learningRate = learningRate;
+        }
+
+        /// <summary>
+        /// Compiles the optimizer
+        /// </summary>
+        /// <param name="graph">Graph to use for compilation</param>
+        /// <param name="loss">Loss function to use</param>
+        /// <param name="parameters">Parameters to optimize</param>
+        public override void Compile(TFGraph graph, TFOutput loss, IEnumerable<TFOutput> parameters)
+        {
+            var operations = new List<TFOperation>();
+
+            var learningRate = graph.Const(_learningRate);
+            var gradients = graph.AddGradients(new[] { loss }, parameters.ToArray());
+
+            foreach (var (parameter, gradient) in Enumerable.Zip(parameters, gradients, (w, g) => (w, g)))
+            {
+                var velocity = graph.Mul(gradient, learningRate);
+                var newWeight = graph.Add(parameter, velocity);
+
+                var operation = graph.Assign(parameter, newWeight).Operation;
+
+                operations.Add(operation);
+            }
+        }
+    }
+}
