d66febeed266ee0794a2dc7a199907a6facd4a5d
Willem Meints <willem.meints@gmail.com>
2018-07-17 15:35:51 +0200

    Add negative log-likelihood loss function

---
+++ src/Neuromatic/Losses/NegativeLogLikelihood.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TensorFlow;
+
+namespace Neuromatic.Losses
+{
+    /// <summary>
+    /// <para>
+    /// The negative log-likelihood loss function is very similar to the categorical cross entropy function.
+    /// It expects the output layer to have a softmax activation function. It calculates the likelihood that
+    /// the output produces a probability distribution that is close to the expected output.
+    /// </para>
+    /// <para>
+    /// The negative log-likelihood is defined as loss = -sum(log(pred)*target)
+    /// </para>
+    /// <para>The predicted probabilities are clipped to prevent this formula
+    /// from producing loss values into infinity.</para>
+    /// </summary>
+    public class NegativeLogLikelihood : LossFunction
+    {
+        private readonly double _epsilon;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="NegativeLogLikelihood"/>
+        /// </summary>
+        /// <param name="epsilon">
+        /// A predefined value by which inputs should be clipped to prevent overflows.
+        /// This value should be really small.
+        /// </param>
+        public NegativeLogLikelihood(double epsilon = 1e-07)
+        {
+            _epsilon = epsilon;
+        }
+
+        /// <summary>
+        /// Compiles the loss function
+        /// </summary>
+        /// <param name="context">Compilation context to use</param>
+        /// <param name="predictions">The output of the graph producing predictions</param>
+        /// <param name="targets">The output of the graph containing the targets</param>
+        /// <returns>Returns the compiled loss function</returns>
+        public override TFOutput Compile(ModelCompilationContext context, TFOutput predictions, TFOutput targets)
+        {
+            // Formula: loss = -sum(log(pred) * target)
+            var graph = context.Graph;
+
+            // Values should be clipped to be between epsilon and 1-epsilon
+            // to prevent arithmic overflows into infinity.
+            var clipped = graph.ClipByValue(predictions, 
+                graph.Const(_epsilon), 
+                graph.Const(1 - _epsilon));
+
+            return graph.Neg(graph.ReduceSum(graph.Mul(targets, graph.Log(clipped))));
+        }
+    }
+}
---
+++ test/Neuromatic.Tests/Losses/NegativeLogLikelihoodTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Neuromatic.Layers;
+using Neuromatic.Losses;
+using TensorFlow;
+using Xunit;
+
+namespace Neuromatic.Tests.Losses
+{
+    public class NegativeLogLikelihoodTests
+    {
+        [Fact]
+        public void CanBeOptimized()
+        {
+            var graph = new TFGraph();
+            var context = new ModelCompilationContext(graph);
+
+            var input = new Input(new long[] { 10 }, name: "Input0");
+            var output = new Dense(2, input, name: "Dense0");
+
+            var compiledOutput = output.Compile(context);
+
+            var loss = new NegativeLogLikelihood();
+
+            var compiledLoss = loss.Compile(context, compiledOutput,
+                context.Graph.Placeholder(TFDataType.Double, new TFShape(-1, 2)));
+
+            var gradients = graph.AddGradients(
+                new[] { compiledLoss },
+                context.Parameters.ToArray());
+        }
+
+        [Fact]
+        public void SolvesTowardsZeroForCorrectCases()
+        {
+            var graph = new TFGraph();
+            var context = new ModelCompilationContext(graph);
+
+            var output = graph.Const(new[] { new[] { 1.0, 0.0 } });
+            var targets = graph.Const(new[] { new[] { 1.0, 0.0 } });
+
+            var loss = new NegativeLogLikelihood();
+
+            var lossFunction = loss.Compile(context, output, targets);
+
+            using (var session = new TFSession(graph))
+            {
+                var error = session.GetRunner().Run(lossFunction);
+
+                Math.Round((double)error.GetValue(), 2).Should().Be(0.0);
+            }
+        }
+
+        [Fact]
+        public void ProducesHighLossForIncorrectCases()
+        {
+            var graph = new TFGraph();
+            var context = new ModelCompilationContext(graph);
+
+            var output = graph.Const(new[] { new[] { 0.0, 0.1 } });
+            var targets = graph.Const(new[] { new[] { 1.0, 0.0 } });
+
+            var loss = new NegativeLogLikelihood();
+
+            var lossFunction = loss.Compile(context, output, targets);
+
+            using (var session = new TFSession(graph))
+            {
+                var error = session.GetRunner().Run(lossFunction);
+                Math.Round((double)error.GetValue(), 2).Should().Be(16.12);
+            }
+        }
+    }
+}
