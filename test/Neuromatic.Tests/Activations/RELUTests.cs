e10b02f31857ccc55dd36694a47950651a8d9653
Willem Meints <willem.meints@gmail.com>
2018-07-28 19:26:30 +0200

    Fix naming issue in tests

---
+++ test/Neuromatic.Tests/Activations/RELUTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Neuromatic.Activations;
+using TensorFlow;
+using Xunit;
+
+namespace Neuromatic.Tests.Activations
+{
+    public class RELUSpec
+    {
+        [Fact]
+        public void ReturnsGraphElement()
+        {
+            var context = new ModelCompilationContext(new TFGraph());
+            var input = context.Graph.Placeholder(TFDataType.Double, new TFShape(10, 10));
+            var output = new RELU().Compile(context, input);
+
+            output.Should().NotBeNull();
+        }
+    }
+}
