31d5d7b412401eabedca05709d088f963a62fc0b
Willem Meints <willem.meints@gmail.com>
2018-07-28 20:02:55 +0200

    Add ones initializer

---
+++ src/Neuromatic/Initializers/Ones.cs
@@ -0,0 +1,21 @@
+﻿using TensorFlow;
+
+namespace Neuromatic.Initializers
+{
+    /// <summary>
+    /// This initialization function assigns ones according to the required shape
+    /// </summary>
+    public class Ones: InitializationFunction
+    {
+        /// <summary>
+        /// Compiles the initialization function
+        /// </summary>
+        /// <param name="graph">Tensorflow graph to use for creating the initialization function</param>
+        /// <param name="shape">The shape of the variable to initialize</param>
+        /// <returns>Returns the compiled initialization function</returns>
+        public override TFOutput Compile(TFGraph graph, TFShape shape)
+        {
+            return graph.Ones(shape);
+        }
+    }
+}
---
+++ test/Neuromatic.Tests/Initializers/OnesTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Neuromatic.Initializers;
+using TensorFlow;
+using Xunit;
+
+namespace Neuromatic.Tests.Initializers
+{
+    public class OnesTests
+    {
+        [Fact]
+        public void GeneratesOutput()
+        {
+            using(var session = new TFSession())
+            {
+                var initializer = new Ones().Compile(session.Graph,new TFShape(5,5));
+                var output = session.GetRunner().Run(initializer);
+
+                output.Shape[0].Should().Be(5);
+                output.Shape[1].Should().Be(5);
+            }
+        }
+    }
+}
