cee3d7a04c60bb2c37c689bd0c18663802c0be56
Willem Meints <willem.meints@gmail.com>
2018-07-29 10:19:03 +0200

    Add linear activation

---
+++ src/Neuromatic/Activations/Linear.cs
@@ -0,0 +1,22 @@
+﻿using TensorFlow;
+
+namespace Neuromatic.Activations
+{
+    /// <summary>
+    /// Linear activation function is a pass-through activation function. It is defined as y = x.
+    /// </summary>
+    public class Linear: ActivationFunction
+    {
+        /// <summary>
+        /// Compiles the activation function
+        /// </summary>
+        /// <param name="context">Use this context to register trainable parameters
+        /// and build the computational graph for the layer</param>
+        /// <param name="input">Input for the activation function</param>
+        /// <returns>Returns the compiled activation function</returns>
+        public override TFOutput Compile(ModelCompilationContext context, TFOutput input)
+        {
+            return context.Graph.Identity(input);
+        }
+    }
+}
---
+++ test/Neuromatic.Tests/Activations/ActivationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using Neuromatic.Activations;
+using TensorFlow;
+using Xunit;
+
+namespace Neuromatic.Tests.Activations
+{
+    public class ActivationTests
+    {
+        [Theory]
+        [InlineData(typeof(Linear))]
+        [InlineData(typeof(RELU))]
+        [InlineData(typeof(Sigmoid))]
+        [InlineData(typeof(Softmax))]
+        public void ReturnsGraphElement(Type functionType)
+        {
+            var activation = (ActivationFunction)Activator.CreateInstance(functionType);
+
+            var context = new ModelCompilationContext(new TFGraph());
+            var input = context.Graph.Placeholder(TFDataType.Double, new TFShape(10, 10));
+            var output = activation.Compile(context, input);
+
+            output.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(typeof(Linear))]
+        [InlineData(typeof(RELU))]
+        [InlineData(typeof(Sigmoid))]
+        [InlineData(typeof(Softmax))]
+        public void ProducesOutput(Type functionType)
+        {
+            var activation = (ActivationFunction)Activator.CreateInstance(functionType);
+
+            var graph = new TFGraph();
+            var context = new ModelCompilationContext(graph);
+            var input = context.Graph.Placeholder(TFDataType.Double, new TFShape(1));
+            var output = activation.Compile(context, input);
+
+            using (var session = new TFSession(graph))
+            {
+                var runner = session.GetRunner();
+
+                runner.AddInput(input, new TFTensor(new[] { new[] { 1.0 } }));
+                var outputValue = runner.Run(output);
+
+                outputValue.GetValue().Should().NotBe(new[] { new[] { 0.0 } });
+            }
+        }
+    }
+}
